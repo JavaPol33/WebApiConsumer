f8e599b81b350d50495e6cbd11a8a47e591d70be
Marcin Szupke <marcin.szupke.sda@gmail.com>
2020-12-12 13:16:36 +0100

    httpurlconnection

---
+++ src/main/java/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MathService mathService = new MathService();
+        try {
+
+
+            System.out.println("Wybierz opcję");
+            System.out.println("1) wylosuj ciekawostkę");
+            System.out.println("2) poznaj informację o liczbie");
+
+            Scanner scanner = new Scanner(System.in);
+            int option = scanner.nextInt();
+            String result;
+
+            switch (option) {
+                case 1:
+                     result = mathService.randomMath();
+                     break;
+                case 2:
+                    System.out.println("Podaj liczbę");
+                    int number = scanner.nextInt();
+                    result = mathService.math(number);
+                    break;
+                default:
+                    result = "Nie rozpoznano wyboru";
+            }
+
+            System.out.println(result);
+
+        } catch (CustomException e) {
+            System.out.println(e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/MathService.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MathService {
+
+    public String randomMath() throws CustomException {
+        String uri = "http://numbersapi.com/random/math";
+        return get(uri);
+    }
+
+    public String math(int number) throws CustomException {
+        String uri = "http://numbersapi.com/" + number;
+        return get(uri);
+    }
+
+    private String get(String uri) throws CustomException {
+        try {
+            URL url = new URL(uri);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while ((line = in.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            in.close();
+
+            if (stringBuilder.toString() == "") {
+                throw new CustomException("Jakiś inny komunikat obłędzie");
+            }
+
+            return stringBuilder.toString();
+
+        } catch (MalformedURLException e) {
+            System.out.println("jakiś błąd");
+            throw new CustomException("jakiś błąd");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
